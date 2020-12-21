3f84f76b87f106480abe233f19df18a861e1f7b4
cmirkhad <60456879+cmirkhad@users.noreply.github.com>
2020-12-22 01:18:02 +0600

    Add files via upload

---
+++ main/js.data/Alatoo.js
@@ -0,0 +1,34 @@
+var data = {
+    "texts" :[
+        {
+            description: "About Ala-Too University",
+            content: "The state governing body of the university is the Ministry of Education and Science of the Kyrgyz Republic. The founder of Ala-Too International University is Sapat International Educational Institutions. Ala-Too International University (AIU) was established in 1996 and it is located in Bishkek, the Kyrgyz Republic. AIU is a legal entity, carries out its activities in accordance with the legislation of the Kyrgyz Republic.\n The governing bodies of the university are:\n - the supreme management body  - Founder;\n- executive body - Rector - Sanzharbek Erdolatov;\n - collegial body - academic council of the university.\n Our university consists of five blocks, where there are classrooms and laboratories equipped with the Internet and modern interactive whiteboards. Our \"Campus\" is an educational center with a library, a conference hall, 3 stadiums, 1 tennis cord, 1 gym, 6 computer classes, 1 Internet club, 1 research center, 2 cantinas, 1 medical center.",
+        },
+        {
+            description: "Mission",
+            content: "Ala-Too International University is located in the Kyrgyz Republic and pursues preparation for competitive, innovative, creative and multilingual professionals. The mission of the university is to prepare well-rounded professionals who respect historical values, have high critical thinking abilities, and are able to adapt to the changing environment through their knowledge and competencies.While preparing professionals, the university sustains human rights and values, information technologies, innovative methods and scientific research that are in line with national traditions. As one of the country's international universities, AIU devotes all resources to develop education, research and innovation.",
+        },
+        {
+            description: "Vision",
+            content: "1) To become one of the top universities in the Central Asia by using innovative and intellectual resources while preparing highly qualified professionals;\n2) University combines the latest achievements in education, science and technology while preparing highly qualified professionals in conforming to society's demands;\n3) Ala-Too International University is an innovative university educating through the latest achievements in education and warrants continuous learning."
+        }, 
+        {
+            description: "Languages in Ala-Too",
+            content: "The main language of instruction is English. Russian, Kyrgyz and Turkish are auxiliary languages. In addition, students of individual faculties can choose a second foreign language, they can choose from Japanese, German, Chinese, French. For those who do not speak English, there are preparatory courses. Upon graduation, the graduates of the University are given a State higher professional education diploma of the Kyrgyz Republic.",
+        },
+        {
+            description: "Staff",
+            content: "The staffing of the AIU is composed of highly qualified, experienced, young, active teachers from Kyrgyzstan, Turkey,  USA, China, India and other countries. Undergraduate, graduate and postgraduate programs are conducted in a multicultural and democratic atmosphere of mutual respect. Our university has bilateral agreements with some universities in Europe, China, America, Turkey and Cyprus.",
+        },
+        {
+            description: "Exchange",
+            content: "We carry out the exchange programs for students and teaching staff in the following programs: Mevliana, Erasmus. Such programs would allow students and teachers an opportunity to continue their studies for 1-2 semesters at various universities and to share their experiences."
+        },
+        {
+            description: "My opinion:)",
+            content: "Ala-Too International University is the best university in Kyrgyzstan."
+        },
+        
+        
+    ]
+}
---
+++ main/js.data/funfacts.js
@@ -0,0 +1,48 @@
+var fundata= {
+    "texts": [
+        {
+            description:"The heads on Easter Island have bodies.",
+            content:"The iconic stone heads protruding from the ground on Easter Island are familiar to most, but many don't realize what lies beneath the surface. In the '10s, archaeologists studying the hundreds of stone statues on the Pacific Island excavated two of the figures, revealing full torsos, which measure as high as 33 feet.",
+        },
+        {
+            description:"The moon has moonquakes.",
+            content:"Just as earth has earthquakes, the moon has-you guessed it-moonquakes. Less common and less intense than the shakes that happen here, moonquakes are believed by U.S. Geological Survey (USGS) scientists to occur due to tidal stresses connected to the distance between the Earth and the moon.",
+        },
+        {
+            description:"Goosebumps are meant to ward off predators.",
+            content:"Why do we get goosebumps? In this physiological reaction, small muscles attached to individual body hairs contract, which leads the hair to stand on end. We inherited this ability from our ancestors in part as a way for our (then) coat of body hair to capture air beneath it and in that way retain heat. But, as George A. Bubenik, a physiologist and professor of zoology at the University of Guelph in Ontario, Canada, explained to Scientific American, it also caused our ancestors to appear bigger than they were, helping to ward off predators when they were frightened or on the defense. With modern humans having less body hair, goosebumps no longer cause us to look that much more intimidating.",
+        },
+        {
+            description:"There's no such thing as \"pear cider.\"",
+            content:"\"Wait,\" you're probably thinking, \"I had pear cider last week.\" Actually, \"cider\" is an alcoholic beverage made from fermented apples, and only apples. Alcoholic beverages can be made from pears, but that drink is known as \"perry.\" The drink was popular in England for centuries but fell out of favor during the second half of the twentieth century. That turned around in the 1990s when the drink was rebranded as pear cider.",
+        },
+        {
+            description:"Pineapple works as a natural meat tenderizer.",
+            content:"The fruit is packed with the enzyme bromelain, which breaks down protein chains, making it an ideal marinade for meats when you don't have a lot of time. But for the same reason, pineapple does not work for jams or jellies, since the enzyme breaks down gelatin as well. The bromelain is so strong that pineapple processors have to wear protective gloves, otherwise over time the enzyme eats away at the skin on their face and hands, leaving dry skin and small sores.",
+        },
+        {
+            description:"Humans are the only animals that blush.",
+            content:"We are also believed to be the only animal that feels embarrassment - a complicated emotion requiring understanding others' opinions and other factors. Charles Darwin called blushing \"the most peculiar and most human of all expressions,\" while Mark Twain said, \"Man is the only animal that blushes. Or needs to.\"",
+        },
+        {
+            description:"The feeling of getting lost inside a mall is known as the Gruen transfer.",
+            content:"We've all heard how casinos are designed to deliberately disorient visitors, causing them to lose track of time and where exactly they are. But did you know that there's a similar strategy behind the design of shopping malls as well? Officially known as the \"Gruen transfer,\" this phenomenon was named after Austrian architect Victor Gruen, who identified how an intentionally confusing layout could lead to consumers spending more time and money in a shopping venue (though he would later disavow the approach).",
+        },
+        {
+            description:"The wood frog can hold its pee for up to eight months.",
+            content:"Talk about having to go! Wood frogs in Alaska have been known to hold their urine for up to eight months, sticking it out through the region's long winters before relieving themselves once temperatures increase. The urine actually helps keep the animal alive while it hibernates, with special microbes in their gut that recycle the urea (urine's main waste) into nitrogen.",
+        },
+        {
+            description:"The hottest spot on the planet is in Libya.",
+            content:"Specifically, the hottest spot ever recorded on Earth is El Azizia, in Libya, where a temperature of 136 degrees Fahrenheit was recorded on Sept. 13, 1922. While hotter spots have likely occurred in other parts of the planet at other times, this is the most scorching temperature ever formally recorded by a weather station.",
+        },
+        {
+            description:"Marie Curie is the only person to earn a Nobel prize in two different sciences.",
+            content:"The pioneering researcher won the Nobel Prize in Physics in 1903 (shared with her husband) for her study of spontaneous radiation, and then won the Nobel Prize in Chemistry in 1911 for her work in radioactivity. That makes her one of just six recipients to receive multiple Nobel prizes, and the only person to receive it in two different sciences. (Chemical engineer Linus Paulding earned a Nobel Chemistry Prize and Nobel Peace Prize, but all other multiple winners received theirs in the same category).20Fingernails don\'t grow ",
+        },
+        {
+            description: "Ruslan agai",
+            content: "Руслан агай, ну поставьте уже 100 баллов:)\nЗнаете как долго я делал этот проект?XD ыхыхы смотрите какой прикольный сайт я сделал! Почти как TypeRacer, but better."
+        }
+    ]
+}
