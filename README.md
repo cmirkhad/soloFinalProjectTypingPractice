38b80b569a0b36f63a14eb8337c846a38dfac16c
Mirkhad <cmirkhad@mail.ru>
2022-05-30 11:44:38 +0600

    final changes

--- README.md
+++ README.md
@@ -5,11 +5,9 @@ My application is based on above ones, also I used some key parts of the code fr
 ## Look of the main page
 ![alt_text](https://github.com/cmirkhad/soloFinalProjectTypingPractice/blob/main/tutorial/webpage.png)
 When you open or reload the page, you will face with random quote to type. To type another quote, just press next quote button.<br>
-On the NavBar there are another texts related to my university and texts with fun facts text to it. Wish you enjoy!)
-## Look of the main page
-![alt_text](https://github.com/cmirkhad/soloFinalProjectTypingPractice/blob/main/tutorial/Alatootextsonpage.png)
+On the NavBar there are another texts with fun facts text to it. Wish you enjoy!)
+
 ## Application
 [here](https://cmirkhad.github.io/soloFinalProjectTypingPractice/main/index.html)
 <br>
 
-See also [Tutorial how to create this app](https://github.com/cmirkhad/soloFinalProjectTypingPractice/blob/main/tutorial/CreateTypingPracticeTutorial.md)
--- main/js/script.js
+++ main/js/script.js
@@ -7,146 +7,144 @@ const WpmElement = document.getElementById('wpm')
 let stop = true;
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-    
-    const arrayValue = quoteInputElement.value.split('')
-    let correct= true;
-    arrayQuote.forEach((ch, index) => {
-    const character = arrayValue[index]
-    
-    if(character == null){
-        ch.classList.remove('incorrect')
-        ch.classList.remove('correct')
-        correct = false;
-    }
-    else if(character === ch.innerText){
-        ch.classList.add('correct')
-        ch.classList.remove('incorrect')
-    }else{
-        ch.classList.remove('correct')
-        ch.classList.add('incorrect')
-        correct = false;
-        
-        
-    }
-    })
-    
-    if(getTimerTime()!=0)WpmElement.innerText=Math.round(arrayValue.length/(getTimerTime()/60)/5)+ " wpm"
-    if(arrayQuote.length==arrayValue.length && correct) {
-        quoteInputElement.disabled = true
-        stop =true;
-        $("#finished").html("Finished!")
-        $("#finished").css("color","green")
-    }
-  
-})
 
+    const arrayValue = quoteInputElement.value.split('')
+    let correct = true;
+    arrayQuote.forEach((ch, index) => {
+        const character = arrayValue[index]
+
+        if (character == null) {
+            ch.classList.remove('incorrect', 'correct')
+            correct = false;
+        } else if (character === ch.innerText) {
+            ch.classList.remove('incorrect')
+            ch.classList.add('correct')
+        } else {
+            ch.classList.remove('correct')
+            ch.classList.add('incorrect')
+
+            correct = false;
+        }
+    });
+
+    if (getTimerTime() != 0) WpmElement.innerText = Math.round(arrayValue.length / (getTimerTime() / 60) / 5) + " wpm";
+
+
+    if (arrayQuote.length == arrayValue.length && correct) {
+        quoteInputElement.disabled = true
+        stop = true;
+        $("#finished").html("Finished!")
+        $("#finished").css("color", "green")
+    }
+
+})
 
 
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
         .then(response => response.json())
         .then(data => data.content)
-    
-}
-
- 
-async function renderNewQuote(){
-    $("#finished").html("")
-    quoteInputElement.disabled = false
-    const quote = await getRandomQuote()
-    $("h3").html("")
-     quoteDisplayElement.innerHTML = ''
-     quote.split('').forEach(character =>{
-         const ch = document.createElement('span')
-         ch.innerText = character
-         quoteDisplayElement.appendChild(ch)
-     })
-     quoteInputElement.value = null
-     WpmElement.innerText="0 wpm"
-     stop=true;
-     timerElement.innerText = 0+ " sec"
-     start=true;
 
 }
 
 
+async function renderNewQuote() {
+    $("#finished").html("");
+    quoteInputElement.disabled = false;
+    const quote = await getRandomQuote();
+    $("h3").html("");
+    quoteDisplayElement.innerHTML = ''
+    quote.split('').forEach(character => {
+        const ch = document.createElement('span')
+        ch.innerText = character;
+        quoteDisplayElement.appendChild(ch)
+    });
+    quoteInputElement.value = null;
+    WpmElement.innerText = "0 wpm";
+    stop = true;
+    timerElement.innerText = 0 + " sec";
+    start = true;
 
+}
 
 
 let startTime
-function startTimer(){
-      
-    
-      stop=false;
-      startTime = new Date()
-      
-      setInterval(() => {
-            if(!stop) timerElement.innerText = getTimerTime()+ " sec"
-          , 1000})
+
+function startTimer() {
+    stop = false;
+    startTime = new Date()
+    setInterval(() => {
+        if (!stop) timerElement.innerText = getTimerTime() + " sec"
+            , 1000
+    })
 }
-function getTimerTime(){
-    return Math.floor((new Date() - startTime)/1000)
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
 }
 
 
-$("#nextbtn").click( function(){
-    
+$("#nextbtn").click(function () {
     renderNewQuote();
 })
-start=true;
-$("#quoteInput").keydown(function(e){
-    if(start) {
+
+
+start = true;
+$("#quoteInput").keydown(function (e) {
+    if (start) {
         startTimer();
-        start=false;
+        start = false;
     }
 })
 
 
-function renderAlaTooText(index){
-    $("#alatoo").append("<li id="+index+"alatoo"+"><a href=\"#\">"+data.texts[index].description+"</a></li>")
-    $("#"+index+"alatoo").click(function(){
-        
-    quoteInputElement.disabled = false
-    const quote = data.texts[index].content;
-    $("h3").html(data.texts[index].description)
-     quoteDisplayElement.innerHTML = ''
-     quote.split('').forEach(character =>{
-         const ch = document.createElement('span')
-         ch.innerText = character
-         quoteDisplayElement.appendChild(ch)
-     })
-     quoteInputElement.value = null
-     WpmElement.innerText="0 wpm"
-     stop=true;
-     timerElement.innerText = 0+ " sec"
-     start=true;
+function renderAlaTooText(index) {
+    $("#alatoo").append("<li id=" + index + "alatoo" + "><a href=\"#\">" + data.texts[index].description + "</a></li>")
+    $("#" + index + "alatoo").click(function () {
+
+        quoteInputElement.disabled = false
+        const quote = data.texts[index].content;
+        $("h3").html(data.texts[index].description)
+        quoteDisplayElement.innerHTML = ''
+        quote.split('').forEach(character => {
+            const ch = document.createElement('span')
+            ch.innerText = character
+            quoteDisplayElement.appendChild(ch)
+        })
+        quoteInputElement.value = null
+        WpmElement.innerText = "0 wpm"
+        stop = true;
+        timerElement.innerText = 0 + " sec"
+        start = true;
     })
 }
 
 
 renderNewQuote();
+
 data.texts.forEach((item, i) => {
-    renderAlaTooText(i);
-}
-); 
-$("#funfactsrandom").click(function(){
-    $("#finished").html("")
-    var ind = Math.floor(Math.random()*fundata.texts.length);
-    quoteInputElement.disabled = false
+        renderAlaTooText(i);
+});
+
+
+$("#funfactsrandom").click(function () {
+    $("#finished").html("");
+    var ind = Math.floor(Math.random() * fundata.texts.length);
+    quoteInputElement.disabled = false;
     const quote = fundata.texts[ind].content;
-    $("h3").html(fundata.texts[ind].description)
-     quoteDisplayElement.innerHTML = ''
-     quote.split('').forEach(character =>{
-         const ch = document.createElement('span')
-         ch.innerText = character
-         quoteDisplayElement.appendChild(ch)
-     })
-     quoteInputElement.value = null
-     WpmElement.innerText="0 wpm"
-     stop=true;
-     timerElement.innerText = 0+ " sec"
-     start=true;
-    
-})
+    $("h3").html(fundata.texts[ind].description);
+    quoteDisplayElement.innerHTML = '';
+    quote.split('').forEach(character => {
+        const ch = document.createElement('span');
+        ch.innerText = character;
+        quoteDisplayElement.appendChild(ch);
+    });
+    quoteInputElement.value = null;
+    WpmElement.innerText = "0 wpm";
+    stop = true;
+    timerElement.innerText = 0 + " sec";
+    start = true;
+
+});
 
 
